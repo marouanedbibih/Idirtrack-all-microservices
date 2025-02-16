c5e924b5f722e69bec1915b8c6c023b863f15322
Marouane Dbibih <marrouane.dbibih@gmail.com>
2025-02-16 16:48:41 +0100

    Remobe unused folders

--- cloud/config-server/src/main/java/com/idirtrack/config_server/ConfigServerApplication.java
+++ cloud/config-server/src/main/java/com/idirtrack/config_server/ConfigServerApplication.java
@@ -1,7 +1,7 @@
 package com.idirtrack.config_server;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
